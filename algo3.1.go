b434d1155ab061962359226f9cb93268a9a9f339
Guilherme Fernandes <guifrxx@gmail.com>
2023-03-16 18:19:54 -0300

    soluçao3.1

---
+++ algo3.1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() { //Faça um algoritmo que leia um número inteiro e mostre se ele é par ou ímpar.
+	var num int
+	fmt.Println("Digite um número")
+	fmt.Scan(&num)
+
+	if num%2 == 0 {
+		fmt.Println("O número é par")
+
+	} else {
+		fmt.Println("O número é impar")
+	}
+}
