b3c116089acbc6be24611b4267edabe88b0c451a
Guilherme Fernandes <guifrxx@gmail.com>
2023-03-16 19:08:17 -0300

    soluçao5.1

---
+++ algo5.1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() { //Faça um algoritmo que leia um número inteiro e mostre se ele é múltiplo de 3 e de 5 ao mesmo tempo.
+	var num int
+	fmt.Println("Digite um número:")
+	fmt.Scan(&num)
+
+	if num%3 == 0 {
+		fmt.Println("É múltiplo de 3")
+	} else if num%5 == 0 {
+		fmt.Println("É múltiplo de 5")
+	} else {
+		fmt.Println("Não é múltiplo de 3 nem de 5")
+	}
+}
