ddbde080e657329c0846f16edee3945701e28ec3
Guilherme Fernandes Monteiro <guimonteiro@dinfor.uniceub>
2023-03-21 09:31:15 -0300

    soluçao5.3

---
+++ algo5.3.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() { //Faça um algoritmo que imprima os números de 10 a 1 em ordem decrescente.
+	for num := 10; num <= 10; num-- {
+		if num == 0 {
+			break
+
+		}
+		fmt.Println(num)
+	}
+
+}
