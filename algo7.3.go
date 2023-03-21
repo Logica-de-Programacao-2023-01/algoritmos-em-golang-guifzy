6def19d967e9e81932e1c6461d466d3b7fc712e4
Guilherme Fernandes Monteiro <guimonteiro@dinfor.uniceub>
2023-03-21 10:02:25 -0300

    soluçao7.3

---
+++ algo7.3.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() { //Faça um algoritmo que imprima os números de 1 a 100, substituindo os múltiplos de 3 pela palavra "Fizz" e os múltiplos de 5 pela palavra "Buzz". Para os números que são múltiplos de ambos, utilize a palavra "FizzBuzz".
+	for num := 1; num <= 100; num++ {
+		if num%3 == 0 && num%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if num%3 == 0 {
+			fmt.Println("Fizz")
+		} else if num%5 == 0 {
+			fmt.Println("Buzz")
+		}
+
+	}
+}
